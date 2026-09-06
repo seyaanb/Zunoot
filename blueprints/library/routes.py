e82544f6c547ff338087a30b868b6c7f32ebf753
Seyaan Budhkar <seyaan.budhkar25@imperial.ac.uk>
2026-09-06 21:53:29 +0100

    Remove Library object tree from Flask session

--- blueprints/library/routes.py
+++ blueprints/library/routes.py
@@ -4,26 +4,24 @@ import backend.queries as q
 import backend.flashcard_library.library as l
 from . import library_bp
 
-
 @library_bp.route("/", methods=["GET", "POST"])
 def library():
     '''
-    a tree (library) of objects (subjects, topics and flashcards) is initialised
-    the tree is added to the session
+    a tree (library) of objects is initialised locally for the request
     the subjects within the library are rendered and an option to add new subjects is given
     '''
     try:
         username = current_user.id
 
-        session["library"] = l.Library(username)
-        session["library"].initialiseLibrary(username)
+        current_library = l.Library(username)
+        current_library.initialiseLibrary(username)
 
-        subjects = session["library"].getChildren()
+        subjects = current_library.getChildren()
 
-        if request and request.method =="POST":
+        if request.method =="POST":
             new_subject_name = request.form.get("new-subject")
             if new_subject_name and not new_subject_name.isspace() and not q.check_subject_exists(new_subject_name, current_user.id):
-                session["library"].addChild(new_subject_name)
+                current_library.addChild(new_subject_name)
                 return redirect(url_for("library.library"))
 
         return render_template("library.html", title= f"{current_user.first_name}'s Library",
@@ -32,7 +30,7 @@ def library():
                                             coins=current_user.get_coins(),
                                             avatar=current_user.get_avatar(),
                                             theme=current_user.get_theme())
-    except:
+    except Exception as e:
         flash("We struggled to load your library. Please try again")
         return redirect(url_for("auth.index"))
 
@@ -41,33 +39,31 @@ def subject(subject_name):
     '''
     users are taken to this route if they click on a subject in the previous route.
     the topics within that subject are rendered and an option to add new topics is given
-
-    param subject_name: a string representing the name of the subject clicked by the user
     '''
-
     try:
-        session["library"].initialiseLibrary(current_user.id)
+        current_library = l.Library(current_user.id)
+        current_library.initialiseLibrary(current_user.id)
         
-        subject = session["library"].getChildByName(subject_name)
-        session["library"].updateCurrentNode(subject)
-        topics = subject.getChildren()
-        
+        subject_node = current_library.getChildByName(subject_name)
+        if not subject_node:
+            return redirect(url_for("library.library"))
+            
+        topics = subject_node.getChildren()
 
-        if request and request.method == "POST":
+        if request.method == "POST":
             new_topic_name = request.form.get("new-topic")
-            if new_topic_name and not new_topic_name.isspace() and not q.check_topic_exists(new_topic_name, subject.subject_id):
-                subject.addChild(new_topic_name)
-                session["library"].initialiseLibrary(current_user.id)
-                return redirect(url_for("library.subject", subject_name=subject.getName()))
+            if new_topic_name and not new_topic_name.isspace() and not q.check_topic_exists(new_topic_name, subject_node.subject_id):
+                subject_node.addChild(new_topic_name)
+                return redirect(url_for("library.subject", subject_name=subject_node.getName()))
         
-        return render_template("library.html", title=subject.getName(),
+        return render_template("library.html", title=subject_node.getName(),
                                             type="subject",
-                                            current_subject=subject,
+                                            current_subject=subject_node,
                                             topics=topics,
                                             coins=current_user.get_coins(),
                                             avatar=current_user.get_avatar(),
                                             theme=current_user.get_theme())
-    except:
+    except Exception as e:
         flash("We struggled to load this subject. Please try again")
         return redirect(url_for("library.library"))
 
@@ -76,36 +72,38 @@ def topic(subject_name, topic_name):
     '''
     users are taken to this route if they click on a topic in the previous route.
     the topics within that subject are rendered and an option to add new flashcards is given
-
-    param topic_name: a string representing the name of the topic clicked by the user
     '''
     try:
-        session["library"].initialiseLibrary(current_user.id)
-        subject = session["library"].getChildByName(subject_name)
-        topic = subject.getChildByName(topic_name)
-        session["library"].updateCurrentNode(topic)
+        current_library = l.Library(current_user.id)
+        current_library.initialiseLibrary(current_user.id)
         
-
-        if request and request.method == "POST":
+        subject_node = current_library.getChildByName(subject_name)
+        if not subject_node:
+            return redirect(url_for("library.library"))
+            
+        topic_node = subject_node.getChildByName(topic_name)
+        if not topic_node:
+            return redirect(url_for("library.subject", subject_name=subject_name))
+        
+        if request.method == "POST":
             current_user.add_coins(10)
             if current_user.get_guild():
                 current_user.add_points(5)
             flashcard_front = request.form.get("new-flashcard-front")
             flashcard_back = request.form.get("new-flashcard-back") 
             if flashcard_front and flashcard_back and not flashcard_front.isspace() and not flashcard_back.isspace():
-                topic.addChild(flashcard_front, flashcard_back)
-                session["library"].initialiseLibrary(current_user.id)
-                return redirect(url_for("library.topic", subject_name=subject.getName(), topic_name=topic.getName()))
+                topic_node.addChild(flashcard_front, flashcard_back)
+                return redirect(url_for("library.topic", subject_name=subject_node.getName(), topic_name=topic_node.getName()))
 
-        flashcards = topic.getChildren()
+        flashcards = topic_node.getChildren()
 
-        return render_template("library.html", title=topic.getName(),
+        return render_template("library.html", title=topic_node.getName(),
                                             type="topic",
                                             flashcards=flashcards,
                                             coins=current_user.get_coins(),
                                             avatar=current_user.get_avatar(),
                                             theme=current_user.get_theme())
-    except:
+    except Exception as e:
         flash("We struggled to load this topic. Please try again")
         return redirect(url_for("library.subject", subject_name=subject_name))
 
@@ -113,14 +111,14 @@ def topic(subject_name, topic_name):
 def search_library():
     '''
     names of subjects and topics, and fronts and backs of flashcards within the tree are searched for a given keyword
-    subjects, topics and/or flashcards found are displayed to the user
     '''
-
     try:
+        current_library = l.Library(current_user.id)
+        current_library.initialiseLibrary(current_user.id)
+        
         keyword = request.args.get("keyword")
-        results = session["library"].searchLibrary(keyword)
+        results = current_library.searchLibrary(keyword)
         subject_list, topic_list, flashcard_list = results
-
 
         return render_template("library_search.html", subjects=subject_list,
                                                     topics=topic_list,
@@ -128,11 +126,10 @@ def search_library():
                                                     coins=current_user.get_coins(),
                                                     avatar=current_user.get_avatar(),
                                                     theme=current_user.get_theme())
-    except:
+    except Exception as e:
         flash("We struggled to load the search results. Please try again")
         return redirect(url_for("library.library"))
     
-
 @library_bp.route("/edit-subject", methods=["POST"])
 def edit_subject():
     '''
@@ -143,12 +140,14 @@ def edit_subject():
         new_name = request.form.get("new-subject-name")
 
         if new_name and old_name != new_name:
-            subject = session["library"].getChildByName(old_name)
-            subject.editName(new_name)
+            current_library = l.Library(current_user.id)
+            current_library.initialiseLibrary(current_user.id)
+            subject_node = current_library.getChildByName(old_name)
+            if subject_node:
+                subject_node.editName(new_name)
 
         return redirect(url_for("library.library"))
-
-    except:
+    except Exception as e:
         flash("Something went wrong. Please try again")
         return redirect(url_for("library.library"))
 
@@ -158,19 +157,24 @@ def edit_topic():
     allows the user to update the name of a topic
     '''
     try:
-        subject = session["library"].current_node
         old_name = request.form.get("old-topic-name")
         new_name = request.form.get("new-topic-name")
 
         if new_name and old_name != new_name:
-            topic = subject.getChildByName(old_name)
-            topic.editName(new_name)
-            session["library"].initialiseLibrary(current_user.id)
+            current_library = l.Library(current_user.id)
+            current_library.initialiseLibrary(current_user.id)
+            
+            # Find the topic anywhere in the library and update it
+            for sub in current_library.getChildren():
+                topic_node = sub.getChildByName(old_name)
+                if topic_node:
+                    topic_node.editName(new_name)
+                    return redirect(url_for("library.subject", subject_name=sub.getName()))
         
-        return redirect(url_for("library.subject", subject_name=subject.getName()))
-    except:
+        return redirect(url_for("library.library"))
+    except Exception as e:
         flash("Something went wrong. Please try again")
-        return redirect(url_for("library.subject", subject_name=subject.getName()))
+        return redirect(url_for("library.library"))
 
 @library_bp.route("/edit-flashcard", methods=["POST"])
 def edit_flashcard():
@@ -178,59 +182,66 @@ def edit_flashcard():
     allows the user to update the front and/or back of a flashcard
     '''
     try:
-        topic = session["library"].current_node
         old_front = request.form.get("old-flashcard-front")
         new_front = request.form.get("new-flashcard-front")
         new_back = request.form.get("new-flashcard-back")
 
-
         if new_front and new_back:
-            flashcard = topic.getChildByName(old_front)
-            flashcard.editFlashcard(new_front, new_back)
-            session["library"].initialiseLibrary(current_user.id)
+            current_library = l.Library(current_user.id)
+            current_library.initialiseLibrary(current_user.id)
+            
+            # Find the flashcard anywhere in the library and update it
+            for sub in current_library.getChildren():
+                for top in sub.getChildren():
+                    flashcard_node = top.getChildByName(old_front)
+                    if flashcard_node:
+                        flashcard_node.editFlashcard(new_front, new_back)
+                        return redirect(url_for("library.topic", subject_name=sub.getName(), topic_name=top.getName()))
         
-        subject = topic.getParent()
-        
-        return redirect(url_for("library.topic", subject_name=subject.getName(), topic_name=topic.getName()))
-    except:
+        return redirect(url_for("library.library"))
+    except Exception as e:
         flash("Something went wrong. Please try again")
-        return redirect(url_for("library.topic", subject_name=subject.getName(), topic_name = topic.getName()))
+        return redirect(url_for("library.library"))
     
-
 @library_bp.route("/delete", methods=["POST"])
 def delete():
     '''
     allows the user to delete a subject, topic or flashcard
     '''
-
     try:
         type = request.form.get("type")
-        id = request.form.get("id")
+        item_id = str(request.form.get("id"))
 
-        parent = session["library"].current_node
-        current_node = parent.getChildByID(id)
-
+        current_library = l.Library(current_user.id)
+        current_library.initialiseLibrary(current_user.id)
 
         if type == "subject":
-            current_node.deleteSubject()
-            flash("Subject deleted")
+            subject_node = current_library.getChildByID(item_id)
+            if subject_node:
+                subject_node.deleteSubject()
+                flash("Subject deleted")
             return redirect(url_for("library.library"))
 
         elif type == "topic":
-            current_node.deleteTopic()
-            session["library"].initialiseLibrary(current_user.id)
-            flash("Topic deleted")
-            return redirect(url_for("library.subject", subject_name=parent.getName()))
-
+            for sub in current_library.getChildren():
+                for top in sub.getChildren():
+                    if str(top.topic_id) == item_id:
+                        top.deleteTopic()
+                        flash("Topic deleted")
+                        return redirect(url_for("library.subject", subject_name=sub.getName()))
 
         elif type == "flashcard":
-            current_node.deleteFlashcard()
-            subject = parent.getParent()
-            session["library"].initialiseLibrary(current_user.id)
-            flash("Flashcard deleted")
-            return redirect(url_for("library.topic", subject_name=subject.getName(), topic_name=parent.getName()))
-    except:
-        ("We couldn't delete the item. Please try again")
+            for sub in current_library.getChildren():
+                for top in sub.getChildren():
+                    for f in top.getChildren():
+                        if str(f.flashcard_id) == item_id:
+                            f.deleteFlashcard()
+                            flash("Flashcard deleted")
+                            return redirect(url_for("library.topic", subject_name=sub.getName(), topic_name=top.getName()))
+                            
+        return redirect(url_for("library.library"))
+    except Exception as e:
+        flash("We couldn't delete the item. Please try again")
         return redirect(url_for("library.library"))
     
 @library_bp.route("/update-order/<itemType>", methods=["POST"])
@@ -240,7 +251,6 @@ def update_order(itemType):
     '''
     order = request.json.get("order")
     q.update_order(itemType, order)
-
     return jsonify({"status": "success"}) 
 
 @library_bp.route('/review', methods=["GET", "POST"])
@@ -253,25 +263,24 @@ def review_page():
         spaced_repetition = request.form.get("spaced-repetition")
 
         if "queue" not in session:
+            current_library = l.Library(current_user.id)
+            current_library.initialiseLibrary(current_user.id)
+            
             if spaced_repetition == "true":
-                session["queue"] = session["library"].makeSessionQueue(True)
+                session["queue"] = current_library.makeSessionQueue(True)
             else:
-                session["queue"] = session["library"].makeSessionQueue(False)
+                session["queue"] = current_library.makeSessionQueue(False)
 
         next_flashcard = session["queue"].getNextFlashcard()
 
-        
         if next_flashcard:
-            front = next_flashcard.getName()
-            back = next_flashcard.getBack()
             return render_template("review.html", title="Review Flashcards",
-                                                front=front,
-                                                back=back,
+                                                front=next_flashcard.getName(),
+                                                back=next_flashcard.getBack(),
                                                 hidden="",
                                                 avatar=current_user.get_avatar(),
                                                 coins=current_user.get_coins(),
                                                 theme=current_user.get_theme())
-        
         
         return render_template("review.html", title="Review Flashcards",
                                             front="No flashcards remaining",
@@ -280,16 +289,15 @@ def review_page():
                                             avatar=current_user.get_avatar(),
                                             coins=current_user.get_coins(),
                                             theme=current_user.get_theme())
-    except:
+    except Exception as e:
         flash("Something went wrong. Please try again")
-        return redirect("url_for('library.library')")
+        return redirect(url_for('library.library'))
 
 @library_bp.route("/spaced-repetition", methods=["GET","POST"])
 def spaced_repetition():
     '''
     takes feedback from the user on their perceived difficulty of the flashcard
     uses this input in the SM2 spaced repetition algorithm to update the priority queue
-    gives users coins and points for reviewing a flashcard
     '''
     try:
         option = request.form.get("option")
@@ -297,12 +305,11 @@ def spaced_repetition():
         current_flashcard.reviewFlashcard(option)
 
         current_user.add_coins(2)
-
         if current_user.get_guild():
             current_user.add_points(1)
 
         return redirect(url_for("library.review_page"))
-    except:
+    except Exception as e:
         flash("Something went wrong. Please try again")
         return redirect(url_for("library.review_page"))
 
@@ -320,6 +327,6 @@ def end_session():
             session.pop("queue", None)
         flash("Your session has been ended")
         return redirect(url_for("library.library"))
-    except:
+    except Exception as e:
         flash("We couldn't end your session.")
         return redirect(url_for("library.library"))
